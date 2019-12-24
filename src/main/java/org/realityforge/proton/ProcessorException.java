8e8b6cdcdb5de7f70dc482e0b648ed7d76e58b3d
Peter Donald <peter@realityforge.org>
2019-12-24 14:42:43 +1100

    Import code form Arez

---
+++ src/main/java/org/realityforge/proton/ProcessorException.java
@@ -0,0 +1,24 @@
+package org.realityforge.proton;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.lang.model.element.Element;
+
+public final class ProcessorException
+  extends RuntimeException
+{
+  @Nonnull
+  private final Element _element;
+
+  public ProcessorException( @Nonnull final String message, @Nonnull final Element element )
+  {
+    super( message );
+    _element = Objects.requireNonNull( element );
+  }
+
+  @Nonnull
+  public Element getElement()
+  {
+    return _element;
+  }
+}
