35bfc088f6a63f3ab120245169e8818b21028ad0
Peter Donald <peter@realityforge.org>
2020-02-13 11:18:32 +1100

    Optimize improts

--- core/src/main/java/org/realityforge/proton/AbstractStandardProcessor.java
+++ core/src/main/java/org/realityforge/proton/AbstractStandardProcessor.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
